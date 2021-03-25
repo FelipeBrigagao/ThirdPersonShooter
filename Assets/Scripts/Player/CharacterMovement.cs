5c68234cecd5797c26458337cc84904f6d8e4fa8
FelipeBrigagao <38021142+FelipeBrigagao@users.noreply.github.com>
2021-03-25 15:56:17 -0300

    Armed Animations

--- Assets/Scripts/Player/CharacterMovement.cs
+++ Assets/Scripts/Player/CharacterMovement.cs
@@ -6,15 +6,12 @@ public class CharacterMovement : MonoBehaviour
 {
 
     Rigidbody rb;
+    Animator anim;
 
     [SerializeField]
     Camera cam;
 
-    float horizontalInput;
-    float verticalInput;
-
     float speed;
-
     float finalSpeed;
     float idleSpeed = 0f;
 
@@ -23,60 +20,89 @@ public class CharacterMovement : MonoBehaviour
    
     [SerializeField]
     float walkingSpeed = 4.5f;
+
+    [SerializeField]
+    float armedWalkSpeed = 3f;
     
     [SerializeField]
     float turnSpeed = 0.15f;
 
     float speedSmoother;
+
     [SerializeField]
-    float increasingSpeedSmoother = 0.2f;
+    float increasingSpeedSmoother = 0.01f;
+
     [SerializeField]
-    float decreasingSpeedSmoother = 0.2f;
+    float decreasingSpeedSmoother = 0.1f;
 
 
     float dirAngle;
     float smoothDirAngle;
 
+    Vector2 inputs;
     Vector3 direction;
     Vector3 moveDirection;
     Vector3 velocity;
     Vector3 moveAmount;
 
     bool idle = true;
+    bool armed = false;
 
-    Animator anim;
+
+    private delegate void ChangeStates();
+    private ChangeStates actualState;
 
 
-    // Start is called before the first frame update
     void Start()
     {
-        rb = this.GetComponent<Rigidbody>();
-        anim = this.GetComponent<Animator>();
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = false;
+
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
         speed = idleSpeed;
 
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        horizontalInput = Input.GetAxisRaw("Horizontal");
-        verticalInput = Input.GetAxisRaw("Vertical");
+        inputs.x = Input.GetAxisRaw("Horizontal");
+        inputs.y = Input.GetAxisRaw("Vertical");
 
-        direction = new Vector3(horizontalInput, 0, verticalInput).normalized;
+        direction = new Vector3(inputs.x, 0, inputs.y).normalized;
 
-        //ver se está correndo
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            armed = !armed;
 
-        ChangeSpeed();
-
-        anim.SetBool("Idle", idle);
-        anim.SetFloat("Speed", speed);
+            anim.SetBool("Armed", armed);
+        }
 
     }
 
 
     private void FixedUpdate()
     {
+        Move();
 
+        if (armed && (actualState != ArmedState))
+        {
+            actualState = ArmedState;
+
+        }else if (!armed && (actualState != UnnarmedState))
+        {
+            actualState = UnnarmedState;
+
+        }
+
+        actualState?.Invoke();
+
+    }
+
+
+    private void Move()
+    {
         if (direction.magnitude >= 0.01f)
         {
             dirAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.transform.eulerAngles.y;
@@ -91,44 +117,75 @@ public class CharacterMovement : MonoBehaviour
 
         moveAmount = velocity * Time.fixedDeltaTime;
 
-        
+
         rb.MovePosition(transform.position + moveAmount);
+
+    }
+
+    void ArmedState()
+    {
+        //ver se está correndo
+
+        anim.SetFloat("Input_x", inputs.x);
+        anim.SetFloat("Input_y", inputs.y);
+
+        ChangeArmedSpeed();
+
+        rb.MoveRotation(Quaternion.Euler(cam.transform.eulerAngles.y * Vector3.up));
+    }
+
+    void UnnarmedState()
+    {
+        anim.SetBool("Idle", idle);
+        anim.SetFloat("Speed", speed);
+
+        //ver se está correndo
+
+        ChangeUnarmedSpeed();
 
         rb.MoveRotation(Quaternion.Euler(Vector3.up * smoothDirAngle));
 
     }
+    
+
+    void ChangeArmedSpeed()
+    {
+        speed = armedWalkSpeed;
+
+    }
 
 
-    void ChangeSpeed()
+
+    void ChangeUnarmedSpeed()
     {
 
-        if(direction.magnitude >= 0.1f )
+        if(direction.magnitude >= 0.1f )    //Se está tendo entrada
         {
-            idle = false;
+            idle = false;                           //Não está idle, para a animação
 
-            if (!Input.GetKey(KeyCode.LeftShift))
+            if (!Input.GetKey(KeyCode.LeftShift))           //Se não está correndo
             {
-                finalSpeed = walkingSpeed;
+                finalSpeed = walkingSpeed;                      // A velocidade a se alcançar é a de "andando"
 
-                if (speed < (walkingSpeed - 0.1f))
+                if (speed < (walkingSpeed - 0.1f))              //Se não está correndo mas está tendo input e a velocidade é menor que a de andando quer dizer que estava parado
                 {
-                    speedSmoother = increasingSpeedSmoother;
+                    speedSmoother = increasingSpeedSmoother;  //Por isso usa o smoother de increasing speed
 
-                }else if(speed > (walkingSpeed + 0.1f))
+                }else if(speed > (walkingSpeed + 0.1f))                 //Se não está correndo mas está tendo input e a velocidade é maior que a de andando quer dizer que estava correndo
                 {
-                    speedSmoother = decreasingSpeedSmoother;
+                    speedSmoother = decreasingSpeedSmoother;      //Por isso usa o smoother de decreasing speed
 
                 }
 
-            }else if (Input.GetKey(KeyCode.LeftShift))
+            }else if (Input.GetKey(KeyCode.LeftShift))          //Se ele está correndo
             {
-                finalSpeed = runningSpeed;
+                finalSpeed = runningSpeed;                                             //Velocidade final de correndo, e não importa se estava parado ou andando, o smoother vai ser de increasing speed
                 speedSmoother = increasingSpeedSmoother;
             }
 
-        }else if (direction.magnitude < 0.1f)
+        }else if (direction.magnitude < 0.1f)                                       //Se não tem entrada 
         {
-            idle = true;
+            idle = true;                                                                   //Quer dizer que quer estar parado, por isso a velocidade deve diminuir para a de idle
 
             finalSpeed = idleSpeed;
             speedSmoother = decreasingSpeedSmoother;
